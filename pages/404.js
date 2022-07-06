f9884615322b5e1059b845152d4849050739a664
Abhay Talreja <abhay@Abhays-MacBook-Pro.local>
2022-07-06 02:02:15 -0400

    Individual page loads content

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Custom404() {
+  return (
+    <section className="text-gray-600 body-font h-96">
+      <div className="container px-5 py-8 mx-auto flex flex-col">
+        <div className="flex flex-grow -m-4 justify-center align-middle mt-40">
+          404 | This page could not be found.
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ pages/course/[slug].js
@@ -0,0 +1,80 @@
+import React from "react"
+import CourseContent from "@/components/Courses/CourseContent"
+import siteConfig from "@/config/siteConfig"
+import CustomHead from "@/components/common/CustomHead"
+import DefaultErrorPage from "next/error"
+import Head from "next/head"
+import ErrorPage from "../404"
+import FireStoreParser from "firestore-parser"
+
+export default function Course({ course }) {
+  console.log(course)
+  return (
+    <>
+      {course ? (
+        <>
+          <div id="content">
+            <CourseContent course={course} />
+          </div>
+        </>
+      ) : (
+        <>
+          <Head>
+            <meta name="robots" content="noindex" />
+          </Head>
+          <ErrorPage statusCode={404} />
+        </>
+      )}
+    </>
+  )
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const searchUrl = `/course/${slug}/`
+
+  console.log(searchUrl)
+  const firestoreQuery = {
+    structuredQuery: {
+      from: [{ collectionId: "courses" }],
+      where: {
+        compositeFilter: {
+          filters: [
+            {
+              fieldFilter: {
+                field: {
+                  fieldPath: "searchUrl",
+                },
+                op: "EQUAL",
+                value: {
+                  stringValue: searchUrl,
+                },
+              },
+            },
+          ],
+          op: "AND",
+        },
+      },
+      limit: 1,
+    },
+  }
+
+  const freeResponse = await fetch(
+    `https://firestore.googleapis.com/v1/projects/thepbcapp/databases/(default)/documents:runQuery`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...firestoreQuery }),
+    }
+  )
+  const freeResponseJson = await freeResponse.json()
+  const courses = await FireStoreParser(freeResponseJson)
+  console.log("courses", courses)
+
+  return {
+    props: {
+      course: courses[0].document.fields,
+    },
+  }
+}
