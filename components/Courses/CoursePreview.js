3bc1d2f7daf03fa4813f64ded2f508f8e3444053
Abhay Talreja <abhay@Abhays-MacBook-Pro.local>
2022-07-07 19:01:49 -0400

    using lighter images

--- components/Courses/CoursePreview.js
+++ components/Courses/CoursePreview.js
@@ -37,7 +37,7 @@ export default function CoursePreview({ course, index }) {
         <Link href={`${course.url.replace(/.*\/\/[^\/]*/, "")}`}>
           <a>
             <Image
-              src={course.images.image_480x270}
+              src={course.images.image_240x135}
               alt={course.title}
               width="480"
               height="270"
