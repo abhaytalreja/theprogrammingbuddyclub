a49acb8ca5361834ff403f4955075ad956ba857a
Abhay Talreja <abhay@Abhays-MacBook-Pro.local>
2022-07-07 18:42:05 -0400

    Adding favicons

--- layouts/defaultLayout.js
+++ layouts/defaultLayout.js
@@ -30,8 +30,8 @@ export default function DefaultLayout({ children }) {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f4d001" />
-        <meta name="msapplication-TileColor" content="#747474" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#fefefe" />
         <meta name="theme-color" content="#ffffff" />
         <meta
           name="p:domain_verify"
