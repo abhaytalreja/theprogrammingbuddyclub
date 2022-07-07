c3d97f88698faee6bb9aa0bc2e12ca952016d4fd
Abhay Talreja <abhay@Abhays-MacBook-Pro.local>
2022-07-07 12:51:21 -0400

    Adding empty category

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -6,5 +6,5 @@ export default {
     "The Programming Buddy Club, showcase of best javascript projects, Made with Vue Js, Made with Angular Js, Made with React Js, Made with Ember Js, Made with Gatsby Js, Made with Meteor Js, Made with Next Js, Made with Node Js, Made with Nuxt Js Showcase example Vue Js, Showcase example Angular Js, Showcase example React Js, Showcase example Ember Js, Showcase example Gatsby Js, Showcase example Meteor Js, Showcase example Next Js, Showcase example Node Js, Showcase example Nuxt Js",
   description:
     "Showcase of the most amazing projects The Programming Buddy Club on planet Earth!",
-  FIRESTORE_KEY: "AIzaSyBwa_ab2bEalespqrXRTT44gSLRcnSYhug",
+  FIRESTORE_KEY: process.env.FIRESTORE_KEY,
 }
