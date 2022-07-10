c49830b3ce15c25c4f4e0f6b10a3742526c682f0
Abhay Talreja <abhay@Abhays-MacBook-Pro.local>
2022-07-10 09:06:31 -0400

    Adding pixel tracking to front end

--- pages/_app.js
+++ pages/_app.js
@@ -6,16 +6,18 @@ import { useRouter } from "next/router"
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("2079597175673530") // facebookPixelId
-        ReactPixel.pageView()
-
-        router.events.on("routeChangeComplete", () => {
+    if (typeof window !== "undefined") {
+      import("react-facebook-pixel")
+        .then((x) => x.default)
+        .then((ReactPixel) => {
+          ReactPixel.init("2079597175673530") // facebookPixelId
           ReactPixel.pageView()
+
+          router.events.on("routeChangeComplete", () => {
+            ReactPixel.pageView()
+          })
         })
-      })
+    }
   }, [router.events])
 
   return (
