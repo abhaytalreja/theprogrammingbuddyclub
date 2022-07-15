d7d324c8d1363ff527d0438bc33ed368a06705e1
Abhay Talreja <abhay@Abhays-MacBook-Pro.local>
2022-07-15 11:50:23 -0400

    Updating for Free courses

--- pages/free-coupon-udemy-courses-today.js
+++ pages/free-coupon-udemy-courses-today.js
@@ -88,6 +88,7 @@ export default function Today({ courses }) {
             .replace(" US", "")
             .replace(",", ".")
             .replace("$", "")
+            .replace("Free", 0)
           // console.log(course.document?.fields.savingPrice, price)
           return accumulator + +price
         }, 0)
