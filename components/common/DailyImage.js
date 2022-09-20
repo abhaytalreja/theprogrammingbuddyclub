8a2ef9a347d20b17778d06c7a21010f97e6843ad
Abhay Talreja <abhay.talreja@gmail.com>
2022-09-20 09:27:02 -0400

    adding colors to image

--- components/common/DailyImage.js
+++ components/common/DailyImage.js
@@ -3,10 +3,48 @@ import * as htmlToImage from "html-to-image"
 import { toPng } from "html-to-image"
 import Image from "next/image"
 
+const items = [
+  "bg-green-100",
+  "bg-red-100",
+  "bg-orange-100",
+  "bg-amber-100",
+  "bg-yellow-100",
+  "bg-lime-100",
+  "bg-emerald-100",
+  "bg-teal-100",
+  "bg-cyan-100",
+  "bg-sky-100",
+  "bg-blue-100",
+  "bg-indigo-100",
+  "bg-violet-100",
+  "bg-purple-100",
+  "bg-fuchsia-100",
+  "bg-pink-100",
+  "bg-rose-100",
+  "bg-green-200",
+  "bg-red-200",
+  "bg-orange-200",
+  "bg-amber-200",
+  "bg-yellow-200",
+  "bg-lime-200",
+  "bg-emerald-200",
+  "bg-teal-200",
+  "bg-cyan-200",
+  "bg-sky-200",
+  "bg-blue-200",
+  "bg-indigo-200",
+  "bg-violet-200",
+  "bg-purple-200",
+  "bg-fuchsia-200",
+  "bg-pink-200",
+  "bg-rose-200",
+]
+
 export default function DailyImage({ savings }) {
+  const bgColor = items[Math.floor(Math.random() * items.length)]
   return (
     <div
-      className="w-full h-156 bg-green-100 flex justify-center"
+      className={`w-full h-156 ${bgColor} flex justify-center`}
       id="today-image"
     >
       <div className="pt-4 px-12 text-center w-full">
