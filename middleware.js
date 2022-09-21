22d4e62a5124db6c64c0843552215b240628761a
Abhay Talreja <abhay.talreja@gmail.com>
2022-09-20 23:00:27 -0400

    removing the utm parameters

--- middleware.js
+++ middleware.js
@@ -8,10 +8,8 @@ export default function middleware(request) {
       request.nextUrl.pathname.indexOf("/go/") + 4
     )
 
-    url =
-      url +
-      request.nextUrl.search +
-      "&utm_source=theProgrammingBuddy&utm_medium=landing_page&utm_campaign=theProgrammingBuddyClub&ref=theProgrammingBuddy.club"
+    url = url + request.nextUrl.search
+    // + "&utm_source=theProgrammingBuddy&utm_medium=landing_page&utm_campaign=theProgrammingBuddyClub&ref=theProgrammingBuddy.club"
 
     return NextResponse.redirect(url)
   }
