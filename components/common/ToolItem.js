f27fe5c116807f7d58a19d8d8a4d36cff33b0a66
Abhay Talreja <abhay.talreja@gmail.com>
2022-09-25 10:50:47 -0400

    Adddding colors for toolsconverters.xyz links

--- components/common/ToolItem.js
+++ components/common/ToolItem.js
@@ -2,8 +2,45 @@ import React from "react"
 import Link from "next/link"
 
 export default function ToolItem({ title, description, url, key }) {
+  const items = [
+    "bg-green-100",
+    "bg-red-100",
+    "bg-orange-100",
+    "bg-amber-100",
+    "bg-yellow-100",
+    "bg-lime-100",
+    "bg-emerald-100",
+    "bg-teal-100",
+    "bg-cyan-100",
+    "bg-sky-100",
+    "bg-blue-100",
+    "bg-indigo-100",
+    "bg-violet-100",
+    "bg-purple-100",
+    "bg-fuchsia-100",
+    "bg-pink-100",
+    "bg-rose-100",
+    "bg-green-50",
+    "bg-red-50",
+    "bg-orange-50",
+    "bg-amber-50",
+    "bg-yellow-50",
+    "bg-lime-50",
+    "bg-emerald-50",
+    "bg-teal-50",
+    "bg-cyan-50",
+    "bg-sky-50",
+    "bg-blue-50",
+    "bg-indigo-50",
+    "bg-violet-50",
+    "bg-purple-50",
+    "bg-fuchsia-50",
+    "bg-pink-50",
+    "bg-rose-50",
+  ]
+  const bgColor = items[Math.floor(Math.random() * items.length)]
   return (
-    <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+    <div className={`flex rounded-lg h-full ${bgColor} p-8 flex-col`}>
       <div className="flex items-center mb-3">
         <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
           <svg
