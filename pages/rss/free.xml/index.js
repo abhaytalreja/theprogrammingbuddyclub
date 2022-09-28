393b26663cbf44510e6ccf46417340cec06477df
Abhay Talreja <abhay.talreja@gmail.com>
2022-09-28 16:52:47 -0400

    Adding Free Rss feed

--- pages/rss/free.xml/index.js
+++ pages/rss/free.xml/index.js
@@ -1,16 +1,16 @@
-import generateRssFeed from "lib/generateRssFeed"
+import generateRssFeed from "lib/generateRssFreeDiscount"
 
 export default function index({ feed }) {
   return feed
 }
 
 export async function getServerSideProps({ res }) {
-  console.log("Generating RSS Feed - Start")
-  const feed = await generateRssFeed()
+  console.log("Generating RSS Feed for Free Courses - Start")
+  const feed = await generateRssFeed(true)
   res.setHeader("content-type", "text/xml")
   res.write(feed) // NOTE: You can also use feed.atom1() or feed.json1() for other feed formats
   res.end()
-  console.log("Generating RSS Feed - Complete")
+  console.log("Generating RSS Feed for Free Courses - Complete")
   return {
     props: {},
   }
