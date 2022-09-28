b0a41bcb562d59f0fb95a1dee45c6a2f81163366
Abhay Talreja <abhay.talreja@gmail.com>
2022-09-28 11:20:42 -0400

    Updating affiliate links, removing console log

--- middleware.js
+++ middleware.js
@@ -3,18 +3,23 @@
 import { NextResponse } from "next/server"
 
 export default function middleware(request) {
+  console.log("Middleware called")
   if (request.nextUrl.pathname.startsWith("/go/")) {
     let url = request.nextUrl.pathname.substring(
       request.nextUrl.pathname.indexOf("/go/") + 4
     )
 
-    url =
-      url +
-      request.nextUrl.search +
-      "&LSNPUBID=i*IXi5qsT7c&ranMID=47901&ranEAID=i*IXi5qsT7c&ranSiteID=i.IXi5qsT7c-7tQalvfFKOIEKn49OkAFyA"
+    const affiliateUrl =
+      "https://click.linksynergy.com/deeplink?id=i*IXi5qsT7c&mid=47901&murl="
+    url = url + request.nextUrl.search
+    // "&LSNPUBID=i*IXi5qsT7c&ranMID=47901&ranEAID=i*IXi5qsT7c&ranSiteID=i.IXi5qsT7c-7tQalvfFKOIEKn49OkAFyA"
     // + "&utm_source=theProgrammingBuddy&utm_medium=landing_page&utm_campaign=theProgrammingBuddyClub&ref=theProgrammingBuddy.club"
 
-    return NextResponse.redirect(url)
+    const encodedUrl = encodeURI(url)
+
+    console.log("Redirect URL" + affiliateUrl + encodedUrl)
+
+    return NextResponse.redirect(affiliateUrl + encodedUrl)
   }
 }
 
