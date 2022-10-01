3c0f701e3898f41c6c1ff0e2bf75ac1f6848d079
Abhay Talreja <abhay.talreja@gmail.com>
2022-10-01 18:28:56 -0400

    Removing affiliate links

--- middleware.js
+++ middleware.js
@@ -3,19 +3,24 @@
 import { NextResponse } from "next/server"
 
 export default function middleware(request) {
-  console.log("Middleware called")
   if (request.nextUrl.pathname.startsWith("/go/")) {
-    let url = request.nextUrl.pathname
-      .substring(request.nextUrl.pathname.indexOf("/go/") + 4)
-      .replace("https:/www", "https://www")
+    // let url = request.nextUrl.pathname
+    //   .substring(request.nextUrl.pathname.indexOf("/go/") + 4)
+    //   .replace("https:/www", "https://www")
 
-    const affiliateUrl =
-      "https://click.linksynergy.com/deeplink?id=i*IXi5qsT7c&mid=47901&murl="
-    // + "&utm_source=theProgrammingBuddy&utm_medium=landing_page&utm_campaign=theProgrammingBuddyClub&ref=theProgrammingBuddy.club"
+    // const affiliateUrl =
+    //   "https://click.linksynergy.com/deeplink?id=i*IXi5qsT7c&mid=47901&murl="
+    // // + "&utm_source=theProgrammingBuddy&utm_medium=landing_page&utm_campaign=theProgrammingBuddyClub&ref=theProgrammingBuddy.club"
 
-    const encodedUrl = encodeURIComponent(url + request.nextUrl.search)
+    // const encodedUrl = encodeURIComponent(url + request.nextUrl.search)
 
-    return NextResponse.redirect(affiliateUrl + encodedUrl)
+    let url = request.nextUrl.pathname.substring(
+      request.nextUrl.pathname.indexOf("/go/") + 4
+    )
+
+    url = url + request.nextUrl.search
+
+    return NextResponse.redirect(url)
   }
 }
 
